4c7f1177c4f999c2419a6231c9bcaf70cd66826b
jiseok08 <heoji0912@gmail.com>
2025-10-17 21:44:49 +0900

    (2025.10.17) 이벤트 버스 패턴

---
+++ Program/Assets/Event Bus Pattern/Scripts/SceneryManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneryManager : MonoBehaviour
+{
+    [SerializeField] Slider slider;
+    [SerializeField] GameObject screen;
+
+    private void Start()
+    {
+        
+    }
+
+    public void LoadScene(int buildIndex)
+    {
+       
+    }
+
+    // public IEnumerator TranstionScene(int index)
+    // {
+    //     slider.value = 0;
+    //     
+    //     screen.SetActive(true);
+    // 
+    //     // <AsyncOperation>
+    //     // - allowSceneActivation
+    //     // 장면이 준비된 즉시 장면이 활성되는 것을 허용하는 변수입니다.
+    // }
+}
