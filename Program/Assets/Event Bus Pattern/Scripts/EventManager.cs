a31d4704ffd83f1fceba9379652125d03ce5d9e8
jiseok08 <heoji0912@gmail.com>
2025-10-21 21:44:09 +0900

    (2025.10.21) 전략 패턴

--- Program/Assets/Event Bus Pattern/Scripts/EventManager.cs
+++ Program/Assets/Event Bus Pattern/Scripts/EventManager.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 
 public enum Condition
 { 
@@ -8,14 +9,35 @@ public enum Condition
     FINISH
 }
 
-
-
 public class EventManager
 {
     public static void Subscribe(Condition condition, Action action)
     {
-        
+        if (dictionary.ContainsKey(condition))
+        {
+            dictionary[condition] += action;
+        }
+        else
+        {
+            dictionary[condition] = action;
+        }
     }
 
-    static Dictionary<Condition, Action> dictionary = new();
+    public static void Unsubscribe(Condition condition, Action action)
+    {
+        if (dictionary.ContainsKey(condition))
+        {
+            dictionary[condition] -= action;
+        }
+    }
+
+    public static void Pubish(Condition condition)
+    {
+        if (dictionary.TryGetValue(condition, out var action))
+        {
+
+        }
+    }
+
+    public static Dictionary<Condition, Action> dictionary = new();
 }
---
+++ Program/Assets/Event Bus Pattern/Scripts/Field.cs
@@ -0,0 +1,7 @@
+using System;
+using UnityEngine;
+
+public class Field : MonoBehaviour
+{
+    
+}
